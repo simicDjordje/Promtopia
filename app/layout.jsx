1f683728b12c9284b9a593800a8a46ef090ed16c
simicdj <66253032+simicDjordje@users.noreply.github.com>
2024-07-02 01:19:42 +0200

    Half of Nav done

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Nav from '@components/Nav'
 import '@styles/globals.css'
 
 export const metadata = {
@@ -14,6 +15,7 @@ const RootLayout = ({children}) => {
             </div>
 
             <main className='app'>
+                <Nav />
                 {children}
             </main>
         </body>
